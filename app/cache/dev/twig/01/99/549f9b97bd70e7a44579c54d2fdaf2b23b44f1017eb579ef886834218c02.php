90470ee694f25282a376cbc5ebbee74a522323b8
sinass <sinayoko.mahamadou@yahoo.fr>
2013-12-09 20:29:34 +0000

    le template de graduate

--- app/cache/dev/twig/01/99/549f9b97bd70e7a44579c54d2fdaf2b23b44f1017eb579ef886834218c02.php
+++ app/cache/dev/twig/01/99/549f9b97bd70e7a44579c54d2fdaf2b23b44f1017eb579ef886834218c02.php
@@ -7,15 +7,36 @@ class __TwigTemplate_0199549f9b97bd70e7a44579c54d2fdaf2b23b44f1017eb579ef8868342
     {
         parent::__construct($env);
 
-        $this->parent = false;
+        $this->parent = $this->env->loadTemplate("::base.html.twig");
 
         $this->blocks = array(
+            'title' => array($this, 'block_title'),
+            'container' => array($this, 'block_container'),
         );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "::base.html.twig";
     }
 
     protected function doDisplay(array $context, array $blocks = array())
     {
-        // line 1
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 3
+    public function block_title($context, array $blocks = array())
+    {
+        // line 4
+        echo "Bonjour : sinass
+";
+    }
+
+    // line 7
+    public function block_container($context, array $blocks = array())
+    {
+        // line 8
         echo "Hello ";
         echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : $this->getContext($context, "name")), "html", null, true);
         echo "!
@@ -34,6 +55,6 @@ class __TwigTemplate_0199549f9b97bd70e7a44579c54d2fdaf2b23b44f1017eb579ef8868342
 
     public function getDebugInfo()
     {
-        return array (  19 => 1,);
+        return array (  40 => 8,  37 => 7,  32 => 4,  29 => 3,);
     }
 }
